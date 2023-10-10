1c6510490d6a3bef2d6b83fe7041a452a1b38229
Florheim <146049699+Florheim@users.noreply.github.com>
2023-10-10 15:22:18 +0800

    Update Activity1.js

---
+++ Activity1.js
@@ -0,0 +1,26 @@
+//ACTIVITY 1
+//ABOUT MYSELF
+let myFirstName = "Florheim Wizard";
+let myMiddleName = "Villena"
+let myLastName = "Mariano";
+let myFullName = myFirstName + " " + myMiddleName + " " + myLastName;
+let mySection = "BSCS 2A";
+
+console.log(myFullName);
+console.log(mySection);
+
+//LET ADD, SUBTRACT, MULTIPLY AND DIVIDE OF MY TWO FAVORITE NUMBER
+let firstFavNum = 100;
+let secondFavNum = 65;
+let getPowerOfFirstFavNum = Math.pow(firstFavNum)
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+
+//CHECK FOR MOD 
+console.log(firstFavNum % secondFavNum);
+
+//LET CHECK THE POWER OF firstFavNum
+console.log(getPowerOfFirstFavNum);
